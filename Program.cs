bcb79ec0c5e263d815e4e9ad147d60f254c348b5
miloszg <milosz.gus@gmail.com>
2020-02-17 16:30:23 +0100

    added main

--- Program.cs
+++ Program.cs
@@ -1,16 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using WebScraper.Builders;
+using WebScraper.Data;
+using WebScraper.Workers;
 
 namespace WebScraper
 {
     class Program
     {
+        private const string Method = "search";
+
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Enter which city you would like to scrape info from:");
+                var cityName = Console.ReadLine();
 
+                Console.WriteLine("Enter which the CraigsList category:");
+                var categoryName = Console.ReadLine();
+
+                using (WebClient client = new WebClient())
+                {
+                    string content = client.DownloadString($"https://{cityName.Replace(" ", string.Empty)}.craigslist.org/{Method}/{categoryName}");
+
+                    ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .WithData(content)
+                        .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.ExplicitCapture)
+                        .WithParts(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .WithParts(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@"href=\""(.*?)\""")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+                    Scraper scraper = new Scraper();
+                    var scraperdElements = scraper.Scrape(scrapeCriteria);
+
+                    if (scraperdElements.Any())
+                    {
+                        foreach (var scrapedElement in scraperdElements)
+                        {
+                            Console.WriteLine(scrapedElement);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("there were no matches for scraping");
+                    }
+                    
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
         }
     }
 }
--- Workers/Scraper.cs
+++ Workers/Scraper.cs
@@ -12,14 +12,14 @@ namespace WebScraper.Workers
     {
         public List<string> Scrape(ScrapeCriteria scrapeCriteria)
         {
-            List<string> scrappedElements = new List<string>();
+            List<string> scrapedElements = new List<string>();
 
             MatchCollection matches = Regex.Matches(scrapeCriteria.Data, scrapeCriteria.Regex, scrapeCriteria.RegexOption);
             foreach(Match match in matches)
             {
                 if (!scrapeCriteria.Parts.Any())
                 {
-                    scrappedElements.Add(match.Groups[0].Value);
+                    scrapedElements.Add(match.Groups[0].Value);
                 }
                 else
                 {
@@ -27,12 +27,12 @@ namespace WebScraper.Workers
                     {
                         Match matchedPart = Regex.Match(match.Groups[0].Value, part.Regex, part.RegexOption);
 
-                        if (matchedPart.Success) scrappedElements.Add(matchedPart.Groups[1].Value);
+                        if (matchedPart.Success) scrapedElements.Add(matchedPart.Groups[1].Value);
                     }
                 }
             }
 
-            return scrappedElements;
+            return scrapedElements;
         }
     }
 }
